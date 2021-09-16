aa2f1a27d849b4fc3d2313af12dac6741b067681
Kerem Guventurk <kerem.guventurk2@gmail.com>
2021-09-16 17:51:08 -0400

    Adding g11 files to g11 branch

---
+++ src/chemotaxis/g11/Agent.java
@@ -0,0 +1,38 @@
+package chemotaxis.g11;
+
+import java.util.Map;
+
+import chemotaxis.sim.DirectionType;
+import chemotaxis.sim.ChemicalCell;
+import chemotaxis.sim.Move;
+import chemotaxis.sim.SimPrinter;
+
+public class Agent extends chemotaxis.sim.Agent {
+
+    /**
+     * Agent constructor
+     *
+     * @param simPrinter  simulation printer
+     *
+     */
+    public Agent(SimPrinter simPrinter) {
+        super(simPrinter);
+    }
+
+    /**
+     * Move agent
+     *
+     * @param randomNum        random number available for agents
+     * @param previousState    byte of previous state
+     * @param currentCell      current cell
+     * @param neighborMap      map of cell's neighbors
+     * @return                 agent move
+     *
+     */
+    @Override
+    public Move makeMove(Integer randomNum, Byte previousState, ChemicalCell currentCell, Map<DirectionType, ChemicalCell> neighborMap) {
+        // TODO add your code here to move the agent
+
+        return null; // TODO modify the return statement to return your agent move
+    }
+}
---
+++ src/chemotaxis/g11/Controller.java
@@ -0,0 +1,78 @@
+package chemotaxis.g11;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+import chemotaxis.sim.ChemicalPlacement;
+import chemotaxis.sim.ChemicalCell;
+import chemotaxis.sim.SimPrinter;
+
+public class Controller extends chemotaxis.sim.Controller {
+
+    /**
+     * Controller constructor
+     *
+     * @param start       start cell coordinates
+     * @param target      target cell coordinates
+     * @param size     	 grid/map size
+     * @param simTime     simulation time
+     * @param budget      chemical budget
+     * @param seed        random seed
+     * @param simPrinter  simulation printer
+     *
+     */
+    public Controller(Point start, Point target, Integer size, Integer simTime, Integer budget, Integer seed, SimPrinter simPrinter) {
+        super(start, target, size, simTime, budget, seed, simPrinter);
+    }
+
+    public int closestToTarget(ArrayList<Point> locations) {
+        int closestDistance = 9999999;
+        int closestIdx = 0;
+        for(int i = 0; i < locations.size(); i++) {
+            int x = locations.get(i).x;
+            int y = locations.get(i).y;
+            int distance = Math.abs(x - this.target.x) + Math.abs(y - this.target.y);
+            if(distance > 0 && distance < closestDistance) {
+                closestIdx = i;
+                closestDistance = distance;
+            }
+        }
+        return closestIdx;
+    }
+
+    /**
+     * Apply chemicals to the map
+     *
+     * @param currentTurn         current turn in the simulation
+     * @param chemicalsRemaining  number of chemicals remaining
+     * @param locations           current locations of the agents
+     * @param grid                game grid/map
+     * @return                    a cell location and list of chemicals to apply
+     *
+     */
+    @Override
+    public ChemicalPlacement applyChemicals(Integer currentTurn, Integer chemicalsRemaining, ArrayList<Point> locations, ChemicalCell[][] grid) {
+        ChemicalPlacement chemicalPlacement = new ChemicalPlacement();
+        int closestIdx = this.closestToTarget(locations);
+        Point currentLocation = locations.get(closestIdx);
+        int currentX = currentLocation.x;
+        int currentY = currentLocation.y;
+
+        int leftEdgeX = Math.max(1, currentX - 5);
+        int rightEdgeX = Math.min(size, currentX + 5);
+        int topEdgeY = Math.max(1, currentY - 5);
+        int bottomEdgeY = Math.min(size, currentY + 5);
+
+        int randomX = this.random.nextInt(rightEdgeX - leftEdgeX + 1) + leftEdgeX;
+        int randomY = this.random.nextInt(bottomEdgeY - topEdgeY + 1) + topEdgeY ;
+
+        List<ChemicalCell.ChemicalType> chemicals = new ArrayList<>();
+        chemicals.add(ChemicalCell.ChemicalType.BLUE);
+
+        chemicalPlacement.location = new Point(randomX, randomY);
+        chemicalPlacement.chemicals = chemicals;
+
+        return chemicalPlacement;
+    }
+}
