bd4a970bcc0b99a4a4410f9f20ef8782fd301ca1
Nikhilesh Belulkar <nb2953@columbia.edu>
2021-09-16 22:06:07 -0400

    Template files all correctly implemented

---
+++ src/chemotaxis/g10/Controller.java
@@ -0,0 +1,43 @@
+package chemotaxis.gx; // TODO modify the package name to reflect your team
+
+import java.awt.Point;
+
+import chemotaxis.sim.ChemicalPlacement;
+import chemotaxis.sim.ChemicalCell;
+import chemotaxis.sim.SimPrinter;
+
+public class Controller extends chemotaxis.sim.Controller {
+
+   /**
+    * Controller constructor
+    *
+    * @param start       start cell coordinates
+    * @param target      target cell coordinates
+    * @param size     	 grid/map size
+    * @param simTime     simulation time
+    * @param budget      chemical budget
+    * @param seed        random seed
+    * @param simPrinter  simulation printer
+    *
+    */
+   public Controller(Point start, Point target, Integer size, Integer simTime, Integer budget, Integer seed, SimPrinter simPrinter) {
+   	super(start, target, size, simTime, budget, seed, simPrinter);
+   }
+
+   /**
+    * Apply chemicals to the map
+    *
+    * @param currentTurn         current turn in the simulation
+    * @param chemicalsRemaining  number of chemicals remaining
+    * @param locations           current locations of the agents
+    * @param grid                game grid/map
+    * @return                    a cell location and list of chemicals to apply
+    *
+    */
+   @Override
+   public ChemicalPlacement applyChemicals(Integer currentTurn, Integer chemicalsRemaining, ArrayList<Point> locations, ChemicalCell[][] grid) {
+      // TODO add your code here to apply chemicals
+
+      return null; // TODO modify the return statement to return your chemical placement
+   }
+}
