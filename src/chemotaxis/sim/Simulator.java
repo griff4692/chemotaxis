a7297ca1b3be72d0029c1f05ed82351e2eb74c20
mns2162 <78117981+mns2162@users.noreply.github.com>
2021-09-20 12:16:42 -0400

    Update Simulator.java

--- src/chemotaxis/sim/Simulator.java
+++ src/chemotaxis/sim/Simulator.java
@@ -609,7 +609,7 @@ public class Simulator {
             throw new IOException("Cannot find the Java class loader!");
 
         @SuppressWarnings("rawtypes")
-        Class rawClass = loader.loadClass("chemotaxis." + teamName + ".Controller");  
+        Class rawClass = loader.loadClass("chemotaxis." + teamName + ".Controller");
         Class[] classArgs = new Class[]{Point.class, Point.class, Integer.class, ChemicalCell[][].class, Integer.class, Integer.class, Integer.class, SimPrinter.class};
 
         return (Controller) rawClass.getDeclaredConstructor(classArgs).newInstance(start, target, mapSize, grid, turns, budget, seed, new SimPrinter(enableControllerPrints));
