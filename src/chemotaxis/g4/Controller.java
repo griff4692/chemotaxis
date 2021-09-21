4a47df0a6aca395c84f9350a72e48f85b2450cbc
jueerEcho <hongwanxiao@gmail.com>
2021-09-21 15:33:39 -0400

    small bug fixed

--- src/chemotaxis/g4/Controller.java
+++ src/chemotaxis/g4/Controller.java
@@ -1,9 +1,9 @@
 package chemotaxis.g4;
 
 import java.awt.Point;
-// import java.util.*;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
+// import java.util.List;
+// import java.util.ArrayList;
 
 import chemotaxis.sim.ChemicalPlacement;
 import chemotaxis.sim.ChemicalCell;
