609553efbb7cc7545db48175b2149744f0997215
Joe Adams <78444947+ja3537@users.noreply.github.com>
2021-09-26 11:24:50 -0400

    altered analysis to put data in arrays

--- src/chemotaxis/g4/Analysis.java
+++ src/chemotaxis/g4/Analysis.java
@@ -3,15 +3,22 @@ import chemotaxis.sim.*;
 import java.awt.Point;
 import java.util.*;
 
+
 public class Analysis {
 
-    ChemicalCell[][] grid;
+    static ChemicalCell[][] grid;
     Double threshold = 0.05;
     ChemicalCell.ChemicalType dye = ChemicalCell.ChemicalType.BLUE;
+    int allowedNotMax = 1;
+    int numGradientCells = 2;
 
 
     public Analysis(ChemicalCell[][] grid){
         this.grid = grid;
+    }
+
+    public void setThreshold(double threshold){
+        this.threshold = threshold;
     }
 
     public static ChemicalCell[][] createNewGrid(ChemicalCell[][] grid){
@@ -51,7 +58,9 @@ public class Analysis {
                         continue;
                     }
                     if(isInBounds(grid.length, row + i, col + j)){
-                        if(grid[row + i][col + j].getConcentration(dye) >= concentration) return false;
+                        if(grid[row + i][col + j].getConcentration(dye) >= concentration){
+                            return false;
+                        }
                     }
                 }
             }
@@ -60,41 +69,84 @@ public class Analysis {
             return false;
         }
         return true;
-
-
     }
 
-    public int simulatePoint(Point p){
-        ChemicalCell[][] newGrid = createNewGrid(grid);
-        int row = p.x - 1;
-        int col = p.y - 1;
-        ChemicalCell cell = newGrid[row][col];
-        int turnCount = 0;
-        int notMaxCount = 0;
-        int allowedNotMax = 1;
-
-        if(cell.isOpen()){
-            cell.applyConcentration(dye);
+    public boolean gradientAlongPath(ChemicalCell[][] newGrid, ArrayList<Point> path, int index){
+        Point p = path.get(index);
+        Point nextP = path.get(index + 1); //next cell along path
+        ChemicalCell cell = newGrid[p.x - 1][p.y - 1];
+        double concentration = cell.getConcentration(dye);
+        double nextPathConcentration = newGrid[nextP.x - 1][nextP.y - 1].getConcentration(dye);
+        if(concentration == 0) return true; //dye has not reached cell yet
+        if(concentration > nextPathConcentration){ //gradient is backward
+            return false;
         }
-
-
-        for(int i = 0; i < 100; i++){
-            if(isMax(newGrid, p)){
-                turnCount++;
-                notMaxCount = 0;
-            }
-            else{
-                notMaxCount++;
-                turnCount++;
-                if(notMaxCount > allowedNotMax) {
-                    return turnCount - (allowedNotMax + 1);
+        for(int i = -1; i <= 1; i++){
+            for(int j = -1; j <= 1; j++){
+                if(i == j || i == -j) continue;
+                if(p.x + i == nextP.x && p.y + j == nextP.y){
+                    continue;
+                }
+                if(isInBounds(newGrid.length, p.x - 1 + i, p.y - 1 + j)) {
+                    if (newGrid[p.x - 1 + i][p.y - 1 + j].getConcentration(dye) > nextPathConcentration){
+                        return false;
+                    }
                 }
             }
-            newGrid = diffuseCells(newGrid);
-
-
         }
-        return turnCount;
+        return true;
+    }
+    public boolean hasGoodGradient(ChemicalCell[][] newGrid, ArrayList<Point> path, int index){
+        if(index < numGradientCells){
+            return false;
+        }
+
+        for(int i = 1; i <= numGradientCells; i++){
+            if(!gradientAlongPath(newGrid, path, index - i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int calculateReached(ChemicalCell[][] newGrid, ArrayList<Point> path, int index){
+        if(index == 0) return 0;
+        for(int i = 1; i <= index; i++){
+            Point p = path.get(index - i);
+            if(newGrid[p.x - 1][p.y - 1].getConcentration(dye) < threshold) return i - 1;
+        }
+        return index;
+    }
+
+    public ArrayList<AnalysisData> analyzePath(ArrayList<Point> path, int arrayLength){
+        ArrayList<AnalysisData> data = new ArrayList<AnalysisData>();
+        for(Point p: path){
+            ChemicalCell[][] newGrid = createNewGrid(grid);
+            int turnCount = 1;
+            int index = path.indexOf(p);
+            int[] reached = new int[arrayLength];
+            boolean[] isMax = new boolean[arrayLength];
+            boolean[] goodGradient = new boolean[arrayLength];
+
+            if(newGrid[p.x - 1][p.y - 1].isOpen()) {
+                newGrid[p.x - 1][p.y - 1].applyConcentration(dye);
+            }
+
+            for(; turnCount <= arrayLength; turnCount++){
+                newGrid = diffuseCells(newGrid);
+                reached[turnCount - 1] = calculateReached(newGrid, path, index);
+                isMax[turnCount - 1] = isMax(newGrid, p);
+                goodGradient[turnCount - 1] = hasGoodGradient(newGrid, path, index);
+            }
+            AnalysisData dataPoint = new AnalysisData(p, arrayLength);
+            dataPoint.distanceReached = reached;
+            dataPoint.isMax = isMax;
+            dataPoint.goodGradient = goodGradient;
+            dataPoint.getIsMaxPercentage();
+            dataPoint.getIsGoodGradientPercentage();
+            data.add(dataPoint);
+        }
+        return data;
     }
 
     private static ChemicalCell[][] diffuseCells(ChemicalCell[][] grid) {
@@ -132,14 +184,6 @@ public class Analysis {
                     double averageConcentration = concentrationSum / numUnblockedCells;
                     if(averageConcentration < 0.001) averageConcentration = 0.0;
                     cell.setConcentration(chemicalType, averageConcentration);
-                    /**
-                    if(averageConcentration > 0){
-                        System.out.println("average concentration is");
-                        System.out.println(averageConcentration);
-                        System.out.println(i);
-                        System.out.println(j);
-                    }
-                     */
                 }
             }
         }
