274c13548eb0d04a621a0217a764234260aecdcf
pbt-santos <pbt.santos@outlook.com>
2021-09-26 15:01:04 -0400

    complete grad find; add first bit chosenChemical

--- src/chemotaxis/g5/Agent.java
+++ src/chemotaxis/g5/Agent.java
@@ -11,29 +11,37 @@ import chemotaxis.sim.Move;
 import chemotaxis.sim.SimPrinter;
 import chemotaxis.sim.ChemicalCell.ChemicalType;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Agent extends chemotaxis.sim.Agent {
-    private Map<Integer, DirectionType> intToDirection;
-    private Map<DirectionType, Integer> directionToInt;
+    private static Map<Integer, DirectionType> intToDirection = 
+                                    Map.of(0, DirectionType.CURRENT, 1, DirectionType.NORTH, 
+                                           2, DirectionType.SOUTH, 3, DirectionType.EAST,
+                                           4, DirectionType.WEST);
+
+    // followed https://stackoverflow.com/questions/20412354/reverse-hashmap-keys-and-values-in-java to reverse
+    private static Map<DirectionType, Integer> directionToInt =
+                                    intToDirection.entrySet()
+                                    .stream()
+                                    .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
     
     public Agent(SimPrinter var1) {
         super(var1);
         // for easier access to our memory mapping to direction. Baeldung referenced for Map of
+        /*
         this.intToDirection = Map.of(0, DirectionType.CURRENT, 1, DirectionType.NORTH, 
                                      2, DirectionType.SOUTH, 3, DirectionType.EAST,
                                      4, DirectionType.WEST);
-        System.out.println(this.intToDirection);
+
         // follow https://stackoverflow.com/questions/20412354/reverse-hashmap-keys-and-values-in-java to reverse
         this.directionToInt =  
             intToDirection.entrySet()
             .stream()
             .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+        */
     }
 
     public Move makeMove(Integer randomNum, Byte previousState, ChemicalCell currentCell, Map<DirectionType, ChemicalCell> neighborMap) {
@@ -43,9 +51,14 @@ public class Agent extends chemotaxis.sim.Agent {
         // how do we change this depending on the path to go to? First chemical is used for priming?
         // then have a swith depending on our selected chemical
 		ChemicalType chosenChemicalType = ChemicalType.GREEN;
-        ChemicalType repulseChemicalType = ChemicalType.RED;
 
-        double highestNetAttraction = 0;
+        /*
+        // get the first bit (from the left) and if it's set then follow blue rather than green
+        if (randomNum >> 7 & 1 == 1) {
+            chosenChemicalType = ChemicalType.BLUE;
+        }
+        */
+        ChemicalType repulseChemicalType = ChemicalType.RED;
 
         /* 
          * Look for a hill to climb if we haven't started, 
@@ -55,11 +68,11 @@ public class Agent extends chemotaxis.sim.Agent {
         // grab required number of bits for direction: https://stackoverflow.com/questions/15255692/grabbing-n-bits-from-a-byte
         // lower three bits represent current direction/gradient direction we're following
         int prevGradientDirection = previousState & 0x7;
-        int backwardsDirection = this.getBackwardsDirection(this.intToDirection.get(prevGradientDirection));
-        int increasingGradDirectionType = this.getIncreasingGradient(currentCell, neighborMap);
+        int backwardsDirection = this.getBackwardsDirection(Agent.intToDirection.get(prevGradientDirection));
+        int increasingGradDirectionType = this.getIncreasingGradient(chosenChemicalType, currentCell, neighborMap);
 
         if (increasingGradDirectionType != 0) {
-            move.directionType = this.intToDirection.get(increasingGradDirectionType);
+            move.directionType = Agent.intToDirection.get(increasingGradDirectionType);
             // unset then set the lowest three bits with the right direction
             // source: https://stackoverflow.com/questions/31007977/how-to-set-3-lower-bits-of-uint8-t-in-c
             // right now we're just using lowest three bits for anything
@@ -71,7 +84,7 @@ public class Agent extends chemotaxis.sim.Agent {
 
         // check if previous direction is available (ie: continue forward)
         if (possibleMoveSet.contains(prevGradientDirection)) {
-            move.directionType = this.intToDirection.get(prevGradientDirection);
+            move.directionType = Agent.intToDirection.get(prevGradientDirection);
             // don't need to change memory since we're continuing
             return move; // done
         }
@@ -79,7 +92,7 @@ public class Agent extends chemotaxis.sim.Agent {
         // if only option is going backwards, size is 1 and we take it
         if (possibleMoveSet.size() == 1) {
             int newMoveDirection = possibleMoveSet.iterator().next();
-            move.directionType = this.intToDirection.get(newMoveDirection);
+            move.directionType = Agent.intToDirection.get(newMoveDirection);
             move.currentState = (byte) newMoveDirection;
             return move;
         }
@@ -90,7 +103,7 @@ public class Agent extends chemotaxis.sim.Agent {
         int count = 0;
         for (Integer dir : possibleMoveSet) {
             if (count == randomNum % possibleMoveSet.size()) {
-                move.directionType = this.intToDirection.get(dir);
+                move.directionType = Agent.intToDirection.get(dir);
                 move.currentState = (byte) dir.intValue();
                 return move;
             }
@@ -101,35 +114,6 @@ public class Agent extends chemotaxis.sim.Agent {
         move.directionType = DirectionType.CURRENT;
         return move;
 
-        // iterate over possible directions and get the net concentration 
-        /*
-        for (DirectionType directionType : neighborMap.keySet()) {
-			// double net = neighborMap.get(directionType).getConcentration(chosenChemicalType) - neighborMap.get(directionType).getConcentration((repulseChemicalType));
-            double net = neighborMap.get(directionType).getConcentration(chosenChemicalType);
-
-            if(net > highestNetAttraction) {
-                highestNetAttraction = net;
-                move.directionType = directionType;
-            }
-		}
-        if (highestNetAttraction == 0){
-             switch(previousState){
-                 case 1:
-                     move.directionType = DirectionType.NORTH;
-                 case 2:
-                     move.directionType = DirectionType.SOUTH;
-                 case 3:
-                     move.directionType = DirectionType.EAST;
-                 case 4:
-                     move.directionType = DirectionType.WEST;
-                 case 0:
-                     move.directionType = DirectionType.CURRENT; // stay in the same location if we haven't set previous direction
-             }
-             if(!neighborMap.keySet().contains(move.directionType)){
-                 move.directionType = neighborMap.keySet().iterator().next();
-             }
-        }
-        */
         // setting specific bits in a byte type: https://stackoverflow.com/questions/4674006/set-specific-bit-in-byte
    }
 
@@ -155,15 +139,29 @@ public class Agent extends chemotaxis.sim.Agent {
         
         for (Map.Entry<DirectionType, ChemicalCell> entry : neighborMap.entrySet()) {
             if (entry.getValue().isOpen()) {
-                possibleMovesSet.add(this.directionToInt.get(entry.getKey()));
+                possibleMovesSet.add(Agent.directionToInt.get(entry.getKey()));
             }
         }
 
         return possibleMovesSet;
     }
 
-    private int getIncreasingGradient(ChemicalCell currentCell, Map<DirectionType, ChemicalCell> neighborMap) {
-        return 0;
+    // get the greatest increasing direction from our neighbouring cells
+    private int getIncreasingGradient(ChemicalType chosenChemicalType, ChemicalCell currentCell, Map<DirectionType, ChemicalCell> neighborMap) {
+        int increasingGradDirection = 0;
+        double highestNetAttraction = 0.0;
+
+        for (Map.Entry<DirectionType, ChemicalCell> entry : neighborMap.entrySet()) {
+			// double net = neighborMap.get(directionType).getConcentration(chosenChemicalType) - neighborMap.get(directionType).getConcentration((repulseChemicalType));
+            double net = entry.getValue().getConcentration(chosenChemicalType);
+
+            if(net > highestNetAttraction) {
+                highestNetAttraction = net;
+                increasingGradDirection = Agent.directionToInt.get(entry.getKey());
+            }
+        }
+
+        return increasingGradDirection;
     }
 
     /*public Move makeMove(Integer var1, Byte var2, ChemicalCell var3, Map<DirectionType, ChemicalCell> var4) {
