01726243f400c5347615bb26da0190311b4a39ee
Wanxiao Hong <hongwanxiao@gmail.com>
2021-09-26 22:50:46 -0400

    Controller with basic placement choosing and refreshing algo

--- src/chemotaxis/g4/Controller.java
+++ src/chemotaxis/g4/Controller.java
@@ -15,7 +15,8 @@ public class Controller extends chemotaxis.sim.Controller {
 	ArrayList<Point> path = null;
 	ArrayList<Point> placementCells = null;
 	ArrayList<ChemicalType> colorPath = null;
-	Integer index = 0;
+	ArrayList<Integer> pcIndexes;
+	ArrayList<Integer> refreshPadding;
 	Analysis analyzer = null;
 
 
@@ -23,6 +24,10 @@ public class Controller extends chemotaxis.sim.Controller {
 	Integer path_interval = 5;
 	Integer chemical_color = 0;
 	Integer arrayLength = 20;
+	Integer pathComplete;
+
+	int spawnFreq = 10;
+	int agentGoal = 3;
 
     /**
      * Controller constructor
@@ -44,12 +49,13 @@ public class Controller extends chemotaxis.sim.Controller {
 			simPrinter.println("creating path");
 			path = getPath(grid);
 		}
+
+		ArrayList<AnalysisData> data;
 		if(analyzer == null){
 			analyzer = new Analysis(grid);
 			analyzer.setThreshold(0.05); //default is 0.05
 
-			ArrayList<AnalysisData> data = analyzer.analyzePath(path, arrayLength);
-
+			data = analyzer.analyzePath(path, arrayLength);
 
 			for(AnalysisData dataPoint:data){
 				System.out.printf("for point %d, %d:\n", dataPoint.p.x, dataPoint.p.y);
@@ -75,10 +81,71 @@ public class Controller extends chemotaxis.sim.Controller {
 				System.out.println();
 				System.out.println();
 			}
+		}
+		else{
+			data = analyzer.analyzePath(path, arrayLength);
+		}
+
+		System.out.print("placement Cells:");
+		placementCells = new ArrayList<Point>();
+		for (int i=1; i<path.size(); i++){
+			if(i%path_interval == 0){
+				int bestPoint = i;
+				// if(data.get(i).p !=  path.get(i)){
+				// 	System.out.println("WARNING!!! NOT EQUAL" + data.get(i).p.x + "," + data.get(i).p.y + " " + path.get(i).x + "," + path.get(i).y);
+				// }
+				if(data.get(bestPoint).isMaxPercentage<data.get(i-1).isMaxPercentage){
+					bestPoint = i-1;
+				}
+				if(data.get(bestPoint).isMaxPercentage<data.get(i+1).isMaxPercentage){
+					bestPoint = i+1;
+				}
+				placementCells.add(path.get(bestPoint));
+				System.out.print(" " + path.get(bestPoint) + " ");
+			}
+		}
+		if((path.size()-1)%path_interval!=0){
+			placementCells.add(path.get(path.size()-1));
+		}
+		System.out.print("\n");
+		if(placementCells.size()>1){
+			pathComplete = 2 * path.indexOf(placementCells.get(placementCells.size()-2)) 
+								- path.indexOf(placementCells.get(placementCells.size()-1)) + path_interval;
+		}
+		else
+			pathComplete = 1;
+		System.out.println("Turns for controller to complete a singel path: "+ pathComplete + "\n");
 
 
+		colorPath = new ArrayList<ChemicalType>();
+		for (int i=0; i<placementCells.size(); i++) {
+			if (i % 3 == 0) {
+				colorPath.add(ChemicalType.RED);
+			} else if (i % 3 == 1) {
+				colorPath.add(ChemicalType.GREEN);
+			} else {
+				colorPath.add(ChemicalType.BLUE);
+			}
+		}
 
-
+		int refreshTimes = Math.min(pathComplete/spawnFreq, agentGoal);
+		pcIndexes = new ArrayList<Integer>();
+		refreshPadding = new ArrayList<Integer>();
+		for(int j=0; j<refreshTimes; j++){
+			if(j==0)
+				refreshPadding.add(1);
+			else{
+				for(int k=2; k<placementCells.size(); k++){
+					int curIndex = path.indexOf(placementCells.get(k));
+					if(curIndex > j*(pathComplete/refreshTimes)){
+						int preIndex = path.indexOf(placementCells.get(k));
+						refreshPadding.add(2 * preIndex - curIndex + path_interval);
+						break;
+					}
+				}
+			}
+			pcIndexes.add(0);
+			System.out.println("refresh padding " + j + " " + refreshPadding.get(j));
 		}
 	}
 
@@ -152,36 +219,57 @@ public class Controller extends chemotaxis.sim.Controller {
 	public ChemicalPlacement applyChemicals(Integer currentTurn, Integer chemicalsRemaining, ArrayList<Point> locations, ChemicalCell[][] grid) {
 
 
-		ChemicalPlacement chemicalPlacement = new ChemicalPlacement();
-		Point p = new Point(11,6);
-		int index = path.indexOf(p);
-		System.out.println("from controller:");
-		System.out.println(grid[p.x - 1][p.y - 1].getConcentration(ChemicalType.BLUE));
-		chemicalPlacement.location = p;
-		ArrayList<ChemicalType> chemicals = new ArrayList<ChemicalType>();
-		if(currentTurn == 1) chemicals.add(ChemicalType.BLUE);
-		else{
-			if(analyzer.hasGoodGradient(grid, path, index)){
-				System.out.printf("still a good gradient on turn: %d\n", currentTurn);
-			}
-			else System.out.printf("not a good gradient on turn: %d\n", currentTurn);
-			if(analyzer.isMax(grid, p)){
-				System.out.printf("still a max on turn: %d\n", currentTurn);
-			}
-			else System.out.printf("not a max on turn: %d\n", currentTurn);
-			System.out.printf("reached on turn %d: %d\n", currentTurn, analyzer.calculateReached(grid, path, index));
+        ChemicalPlacement chemicalPlacement = new ChemicalPlacement();
+ 
+        simPrinter = new SimPrinter(true);
+ 
+        return determineLocation(currentTurn, 0);
 
+	}
+
+	ChemicalPlacement determineLocation(Integer currentTurn, Integer idx){
+		ChemicalPlacement chemicalPlacement = new ChemicalPlacement();
+
+		int pcIndex = pcIndexes.get(idx);
+		Point currentLocation = placementCells.get(pcIndex);
+        int curIndex = path.indexOf(currentLocation);
+ 
+        int turnToPlace = refreshPadding.get(idx);
+        if(pcIndexes.get(idx)==0){
+            turnToPlace += 1;
+        }
+        else{
+            int preIndex = path.indexOf(placementCells.get(pcIndexes.get(idx)-1));
+            turnToPlace += 2 * preIndex - curIndex + path_interval;
+        }
+		System.out.println("idx " + idx);
+		System.out.println("pcIndexes[idx] " + pcIndex);
+		System.out.println("currentLocation " + currentLocation);
+		System.out.println("turnToPlace " + turnToPlace);
+		System.out.println("currentTurn " + currentTurn);
+        
+        if(currentTurn >= turnToPlace){
+            chemicalPlacement.location = currentLocation;
+            ArrayList<ChemicalType> chemicals = new ArrayList<>();
+            chemicals.add(colorPath.get(pcIndex));
+            chemicalPlacement.chemicals = chemicals;
+            pcIndexes.set(idx, ++pcIndex);
+			System.out.println("Chemical Placed at " + currentLocation);
+			if(pcIndex>=placementCells.size()){
+				int padding = refreshPadding.get(idx);
+				if(idx!=0)
+					System.err.println("WARNING: Chemical placement sequence messed up");
+				pcIndexes.remove(0);
+				refreshPadding.remove(0);
+				pcIndexes.add(pcIndex%placementCells.size());
+				refreshPadding.add(padding + (currentTurn/pathComplete) * pathComplete);
+			}
+        }
+		else{
+			if(pcIndexes.get(idx)/3 > pcIndexes.get((idx+1)%pcIndexes.size())/3)
+				chemicalPlacement = determineLocation(currentTurn, idx+1);
 		}
-		chemicalPlacement.chemicals = chemicals;
 
 		return chemicalPlacement;
-
-		//simPrinter = new SimPrinter(true);
-
-
-
-
-
-		//return chemicalPlacement;
 	}
 }
