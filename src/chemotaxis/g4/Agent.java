2fe1bdc37c8af8fa556bc8f12f1ab8724dc91e7e
Alexander Peile <apeile@Alexanders-MacBook-Pro.local>
2021-09-26 20:54:25 -0400

    Implemented voting mechanic for rolling

--- src/chemotaxis/g4/Agent.java
+++ src/chemotaxis/g4/Agent.java
@@ -34,21 +34,29 @@ public class Agent extends chemotaxis.sim.Agent {
 	public Move makeMove(Integer randomNum, Byte previousState, ChemicalCell currentCell, Map<DirectionType, ChemicalCell> neighborMap) {
 		Move move = new Move();
         // Chop up the previous state into stored info:
-        // 1st bit: if the agent is rolling
-        // 2nd bit: unused for now
-        // 3-4 bits: determine current chem - 00=RED,01=GREEN,10=BLUE
-        // 5-6 bits: determine second last move - 00=WEST,01=EAST,10=NORTH,11=SOUTH
+        // 1-2nd bits: determine current chem - 00=INITIAL STATE (RED,NO ROLLING),01=RED,10=GREEN, 11=BLUE
+        // 3-4 bits: determine third last move - 00=WEST,01=EAST,10=NORTH,11=SOUTH
+        // 5-6 bits: determine second last move
         // 7-8 bits: determine last move
-        Boolean isRolling = previousState < 0;
-        int chemIdx = (48 & previousState) >> 4;
+        int chemIdx = (192 & previousState) >> 6;
+        int thdLastMoveIdx = (48 & previousState) >> 4;
         int secLastMoveIdx = (12 & previousState) >> 2;
         int lastMoveIdx = 3 & previousState;
 
         ChemicalType chosenChemicalType = switch (chemIdx) {
             case 0 -> ChemicalType.RED;
-            case 1 -> ChemicalType.GREEN;
-            case 2 -> ChemicalType.BLUE;
+            case 1 -> ChemicalType.RED;
+            case 2 -> ChemicalType.GREEN;
+            case 3 -> ChemicalType.BLUE;
             default -> ChemicalType.RED;
+        };
+
+        DirectionType thdLastMove = switch (thdLastMoveIdx) {
+            case 0 -> DirectionType.WEST;
+            case 1 -> DirectionType.EAST;
+            case 2 -> DirectionType.NORTH;
+            case 3 -> DirectionType.SOUTH;
+            default -> DirectionType.EAST;
         };
 
         DirectionType secLastMove = switch (secLastMoveIdx) {
@@ -72,34 +80,30 @@ public class Agent extends chemotaxis.sim.Agent {
 			if (highestConcentration < neighborMap.get(directionType).getConcentration(chosenChemicalType)) {
 				highestConcentration = neighborMap.get(directionType).getConcentration(chosenChemicalType);
 				move.directionType = directionType;
-                isRolling = Boolean.FALSE;
 			}
 		}
-        // If there is none of the sought chemical and the agent is rolling,
-        // chose between last two moves with 50/50 prob
-        if (isRolling){
-            int rollDir = randomNum%2;
-            if (rollDir==0){
+        // If there is none of the sought chemical and the agent is not in the initial state,
+        // chose any direction that occured in the last 2 moves
+        if (highestConcentration == 0 && chemIdx != 0){
+            if(lastMove == secLastMove){
                 move.directionType = lastMove;
             }
-            else{
+            else if(lastMove == thdLastMove){
+                move.directionType = lastMove;
+            }
+            else if (secLastMove == thdLastMove){
                 move.directionType = secLastMove;
             }
         }
         //If the agent is at local maxima, update currState
         int currState = 0;
-        if(move.directionType == DirectionType.CURRENT){
-            currState = -128;
+        if(highestConcentration == 0){
             int currChem = switch (chosenChemicalType) {
                 case RED -> 1;
                 case GREEN -> 2;
                 case BLUE -> 0;
             };
-            currState = currState | currChem<<4 | secLastMoveIdx<<2 | lastMoveIdx;
-        }
-        //Construct current state byte
-        else if (isRolling) {
-            currState = previousState;
+            currState = currChem<<6 | thdLastMoveIdx<<4 | secLastMoveIdx<<2 | lastMoveIdx;
         }
         else{
             int currChem = switch (chosenChemicalType) {
@@ -108,6 +112,7 @@ public class Agent extends chemotaxis.sim.Agent {
                 case BLUE -> 2;
             };
             int currSecLastMove = lastMoveIdx;
+            int currThdLastMove = secLastMoveIdx;
             int currLastMove = switch (move.directionType){
                 case WEST -> 0;
                 case EAST -> 1;
@@ -115,7 +120,7 @@ public class Agent extends chemotaxis.sim.Agent {
                 case SOUTH -> 3;
                 case CURRENT -> lastMoveIdx;
             };
-            currState = (currChem<<4) | (currSecLastMove<<2) | (currLastMove);
+            currState = (currChem<<6) | (currThdLastMove<<4) | (currSecLastMove<<2) | (currLastMove);
         }
         move.currentState = (byte) currState;
 
